7c6c01d7c36c0eaf84c00784595dd0fc4ae3f47b
DrakkarRyu <javisodst-metal@outlook.com>
2022-05-29 14:28:03 -0500

    middleware product exist, not protect owner

--- src/middlewares/products.middlewares.js
+++ src/middlewares/products.middlewares.js
@@ -1,6 +1,15 @@
 const { catchAsync } = require('../utils/catchAsync');
+const { Product } = require('../models/product.model');
 
 const protectProductOwner = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await Product.findOne({ where: { id } });
+
+  if (!product) {
+    return next(new AppError('Not exist a product with that id', 404));
+  }
+  req.product = product;
   next();
 });
 
