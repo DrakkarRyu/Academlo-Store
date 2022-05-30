7886a523f06cca32858faaf4d2cf8a6f0ee7f0f7
DrakkarRyu <javisodst-metal@outlook.com>
2022-05-30 17:26:04 -0500

    addproduct

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -1,7 +1,35 @@
 const { catchAsync } = require('../utils/catchAsync');
+const { Cart } = require('../models/cart.model');
+const { ProductInCart } = require('../models/productInCart.model');
+const { Product } = require('../models/product.model');
 
 const addProductToCart = catchAsync(async (req, res, next) => {
-  res.status(200).json({ status: 'success' });
+  const { productId, quantity } = req.body;
+  const { sessionUser } = req;
+
+  const cart = await Cart.findOne({
+    where: { status: 'active' },
+  });
+  if (!cart) {
+    const newCart = await Cart.create({
+      userId: sessionUser.id,
+    });
+    res.status(201).json({
+      newCart,
+    });
+  }
+
+  const newProductInCart = await ProductInCart.create({
+    include: [{ model: Product }],
+
+    cartId: cart.id,
+    productId,
+    quantity,
+  });
+
+  res.status(201).json({
+    newProductInCart,
+  });
 });
 
 const updateProductInCart = catchAsync(async (req, res, next) => {
